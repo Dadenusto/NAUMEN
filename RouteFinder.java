547f8ac0a2189d90dccc605e9806dfba5a62533a
Dadenusto <55355358+Dadenusto@users.noreply.github.com>
2021-06-03 16:41:57 +0300

    2.0

--- RouteFinder.java
+++ RouteFinder.java
@@ -1,4 +1,9 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Интерфейс поиска маршрута
@@ -34,153 +39,185 @@ public interface RouteFinder {
         // new_map для поиска пути
         int[][] new_map = new int[map.length][map[0].length];
         // матрица 2x2 для координат @ и Х
-        int[][] start_goal = new int[2][2];
+        int[] start_goal = new int[4];
+        int line_count = map.length;
+        int row_count =  map[0].length;
         // цикл для построения new_map и поиска координат начала и конца
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (map[i][j] == '@') {
-                    // если @, то в new_map на этой координате значение 0, а первая строка в start_goal = ее координатам
-                    new_map[i][j] = 0;
-                    start_goal[0][0] = i;
-                    start_goal[0][1] = j;
-                } else if (map[i][j] == '.')
-                    // если ., то в new_map на этой координате значение -1
-                    new_map[i][j] = -1;
-                else if (map[i][j] == '#')
-                    // если #, то в new_map на этой координате значение -2
-                    new_map[i][j] = -2;
-                else if (map[i][j] == 'X') {
-                    // если Х, то в new_map на этой координате значение -3, а вторая строка в start_goal = ее координатам
-                    new_map[i][j] = -3;
-                    start_goal[1][0] = i;
-                    start_goal[1][1] = j;
-                }
-            }
-        }
-        // массив для координаты, из которой придем в X
-        int way[]= {-1, -1};
+        IntStream.range(0, line_count)
+                .forEach(i -> {
+                    IntStream.range(0, row_count)
+                            .forEach(j-> {
+                                switch (map[i][j]) {
+                                    case '.':
+                                        // если ., то в new_map на этой координате значение -1
+                                        new_map[i][j] = -1;
+                                        break;
+                                    case '#':
+                                        // если #, то в new_map на этой координате значение -2
+                                        new_map[i][j] = -2;
+                                        break;
+                                    case 'X':
+                                        // если Х, то в new_map на этой координате значение -3, а в start_goal элементах 3 и 4 = ее координатам
+                                        new_map[i][j] = -3;
+                                        start_goal[2] = i;
+                                        start_goal[3] = j;
+                                        break;
+                                    case '@':
+                                        // если @, то в new_map на этой координате значение 0, а start_goal элементах 1 и 2 = ее координатам
+                                        new_map[i][j] = 0;
+                                        start_goal[0] = i;
+                                        start_goal[1] = j;
+                                        break;
+                                }
+                    });
+        });
+        // координаты, из которой придем в X
+        int way1=-1;
+        int way2=-1;
         // переменная для проверки, построились ли соседние клетки
-        boolean stop;
         // параметр для построения
         int d = 0;
-        do {
-            // считаем, что не в сосдних координатах не построилось
-            stop = true;
-            // проходим все точки
-            for (int i = 0; i < map.length && way[0]==-1 && way[1]==-1; i++) {
-                for (int j = 0; j < map[i].length && way[0]==-1 && way[1]==-1; j++){
-                    // если значение в точке равно проверяемому значению, то ставим значения для соседних с ней
-                    if (new_map[i][j] == d)
-                        stop = Create_waves(map, new_map, way, stop, d, i, j);
+        // координаты точки 0
+        int[]arr =  new int[]{start_goal[0], start_goal[1]};
+        int arr_length=arr.length;
+        boolean stop = false;
+        do{
+            // массив для точек d+1
+            int []new_arr = new int[(d+1)*4*2];
+            int g = 0;
+            stop = false;
+            for (int k = 0; k<arr_length;k+=2) {
+                // берем точку, в которых расположенно d
+                int i = arr[k];
+                int j = arr[k+1];
+                if (new_map[i][j] == d) {
+                    if (i - 1 >= 0 && way2 == -1) {
+                        // если она не имеет значение, записываем в нее значение на 1 больше
+                        switch (new_map[i - 1][j]) {
+                            case -1 -> {
+                                new_map[i - 1][j] = d + 1;
+                                new_arr[g]= i - 1;
+                                new_arr[g+1]= j;
+                                g+=2;
+                                stop = true;
+                            }
+                            case -3 -> {
+                                way1 = i;
+                                way2 = j;
+                                break;
+                            }
+                        }
+                    }
+                    // если существует точка ниже данной и не найден путь до точки
+                    if (i + 1 <= line_count - 1 && way2 == -1) {
+                        switch (new_map[i + 1][j]) {
+                            case -1 -> {
+                                new_map[i + 1][j] = d + 1;
+                                new_arr[g]= i + 1;
+                                new_arr[g+1]= j;
+                                g+=2;
+                                stop = true;
+                            }
+                            case -3 -> {
+                                way1 = i;
+                                way2 = j;
+                                break;
+                            }
+                        }
+                    }
+                    // если существует точка левее данной и не найден путь до точки
+                    if (j - 1 >= 0 && way2 == -1) {
+                        switch (new_map[i][j - 1]) {
+                            case -1 -> {
+                                new_map[i][j - 1] = d + 1;
+                                new_arr[g]= i;
+                                new_arr[g+1]= j-1;
+                                g+=2;
+                                stop = true;
+                            }
+                            case -3 -> {
+                                way1 = i;
+                                way2 = j;
+                                break;
+                            }
+                        }
+                    }
+                    // если существует точка правее данной и не найден путь до точки
+                    if (j + 1 <= row_count - 1 && way2 == -1) {
+                        switch (new_map[i][j + 1]) {
+                            case -1 -> {
+                                new_map[i][j + 1] = d + 1;
+                                new_arr[g]= i;
+                                new_arr[g+1]= j + 1;
+                                g+=2;
+                                stop = true;
+                            }
+                            case -3 -> {
+                                way1 = i;
+                                way2 = j;
+                                break;
+                            }
+                        }
+                    }
                 }
             }
+            arr = new_arr;
+            arr_length=arr.length;
             d++;
-        } while (!stop && way[0]==-1);
-        /*for(int[] m: new_map){
-            for(int s: m){
-                System.out.print(s+" ");
-            }
-            System.out.println();
-        }*/
-        if (way[0] != - 1 && way[1] != -1) {
+        } while (stop && way1 == -1);
+        if (way1 != - 1) {
+            int nm = new_map[way1][way2];
             while (true) {
                 // если существует точка выше исходной
-                if (way[0] - 1 >= 0){
+                if (way1 - 1 >= 0) {
                     // если ее значение на 1 меньше
-                    if (new_map[way[0]][way[1]] == new_map[way[0]-1][way[1]] + 1) {
+                    if (nm == new_map[way1 - 1][way2] + 1) {
                         // точка в map c данными координатами  становится '+'
-                        map[way[0]][way[1]] = '+';
+                        map[way1][way2] = '+';
                         //если точка выше равна @, то останавливаемся
-                        if (map[way[0]-1][way[1]] == '@')
+                        if (map[way1 - 1][way2] == '@')
                             break;
                         // переходим в точку выше
-                        way[0]--;
+                        way1--;
+                        nm = new_map[way1][way2];
                     }
                 }
                 // если существует точка ниже исходной
-                if (way[0] + 1 <= map.length - 1){
-                    if (new_map[way[0]][way[1]] == new_map[way[0]+1][way[1]] + 1) {
-                        map[way[0]][way[1]] = '+';
-                        if (map[way[0]+1][way[1]] == '@')
+                if (way1 + 1 <= map.length - 1) {
+                    if (nm == new_map[way1 + 1][way2] + 1) {
+                        map[way1][way2] = '+';
+                        if (map[way1 + 1][way2] == '@')
                             break;
-                        way[0]++;
+                        way1++;
+                        nm = new_map[way1][way2];
                     }
                 }
                 // если существует точка левее исходной
-                if (way[1] - 1 >= 0) {
-                    if (new_map[way[0]][way[1]] == new_map[way[0]][way[1]-1] + 1) {
-                        map[way[0]][way[1]] = '+';
-                        if (map[way[0]][way[1]-1] == '@')
+                if (way2 - 1 >= 0) {
+                    if (nm == new_map[way1][way2 - 1] + 1) {
+                        map[way1][way2] = '+';
+                        if (map[way1][way2 - 1] == '@')
                             break;
-                        way[1]--;
+                        way2--;
+                        nm = new_map[way1][way2];
                     }
                 }
                 // если существует точка правее исходной
-                if (way[1] + 1 <= map[0].length - 1) {
-                    if (new_map[way[0]][way[1]] == new_map[way[0]][way[1]+1] + 1) {
-                        map[way[0]][way[1]] = '+';
-                        if (map[way[0]][way[1]+1] == '@')
+                if (way2 + 1 <= map[0].length - 1) {
+                    if (nm == new_map[way1][way2 + 1] + 1) {
+                        map[way1][way2] = '+';
+                        if (map[way1][way2 + 1] == '@')
                             break;
-                        way[1]++;
+                        way2++;
+                        nm = new_map[way1][way2];
                     }
                 }
             }
             // возвращаем map с построенным маршрутом
             return map;
-            }
-        else
-            // если маршрут не найден возвращаем null
-            return null;
         }
-
-    static boolean Create_waves(char[][] map, int[][] new_map, int[] way, boolean stop, int d, int i, int j) {
-            // если существует точка выше данной
-            if (i - 1 >= 0 && way[1]==-1) {
-                // если она не имеет значение, записываем в нее значение на 1 больше
-                if (new_map[i - 1][j] == -1) {
-                    new_map[i - 1][j] = d + 1;
-                    stop = false;
-                }
-                // если эта точка - конец маршрута, то запаминаем точку, из которой пришли
-                else if (new_map[i - 1][j] == -3){
-                    way[0]=i;
-                    way[1]=j;
-                }
-            }
-            // если существует точка ниже данной и не найден путь до точки
-            if (i + 1 <= map.length - 1 && way[1]==-1) {
-                //stop = Create_wave(new_map, way, stop, d, i+1, j);
-                if (new_map[i + 1][j] == -1) {
-                    new_map[i + 1][j] =  d + 1;
-                    stop = false;
-                }
-                else if (new_map[i + 1][j] == -3){
-                    way[0]=i;
-                    way[1]=j;
-                }
-            }
-            // если существует точка левее данной и не найден путь до точки
-            if (j - 1 >= 0 && way[1]==-1) {
-                if (new_map[i][j - 1] == -1) {
-                    new_map[i][j - 1] = d + 1;
-                    stop = false;
-                }
-                else if (new_map[i][j - 1] == -3){
-                    way[0]=i;
-                    way[1]=j;
-                }
-            }
-            // если существует точка правее данной и не найден путь до точки
-            if (j + 1 <= map[i].length - 1 && way[1]==-1) {
-                if (new_map[i][j + 1] == -1) {
-                    new_map[i][j + 1] = d + 1;
-                    stop = false;
-                }
-                else if (new_map[i][j + 1] == -3){
-                    way[0]=i;
-                    way[1]=j;
-                }
-            }
-        return stop;
+        else {
+            return new char[][]{{'n', 'u', 'l', 'l'}};
+        }
     }
 }
